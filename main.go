079fd7fd5de3c06ffda586648041881e088c6f97
Davis Osiemo <davisossy@gmail.com>
2024-11-26 20:55:04 +0300

    data processing

---
+++ main.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+
+	//"log"
+
+	"os"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Logger.Printf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+
+	router := gin.Default()
+
+	marketsProcessor()
+
+	resultsProcessor()
+
+	fixturesProcessor()
+
+	os.Setenv("PORT", "8081")
+
+	if err := router.Run(fmt.Sprintf(":%s", os.Getenv("PORT"))); err != nil {
+		log.Fatal().Err(err).Msg("Startup failed")
+	}
+
+}
+
+func marketsProcessor() {
+
+	conn, err := amqp.Dial("amqp://liden:lID3n@10.132.0.28:5672/")
+	//conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"MARKETS_QUEUE", // name
+		false,           // durable
+		false,           // delete when unused
+		false,           // exclusive
+		false,           // no-wait
+		nil,             // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	fmt.Println("Consumed markets: ", msgs)
+
+	// var forever chan struct{}
+
+	// var upgrader = websocket.Upgrader{
+	// 	ReadBufferSize:  1024,
+	// 	WriteBufferSize: 1024,
+	// }
+
+	// upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	// ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+	// defer ws.Close()
+
+	// go func() {
+	// 	for d := range msgs {
+	// 		log.Printf("Received a message: %s", d.Body)
+	// 		ws.WriteMessage(websocket.TextMessage, []byte(d.Body))
+
+	// 	}
+	// }()
+
+	// log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	// <-forever
+}
+
+func resultsProcessor() {
+
+	conn, err := amqp.Dial("amqp://liden:lID3n@10.132.0.28:5672/")
+	//conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"RESULTS_QUEUE", // name
+		false,           // durable
+		false,           // delete when unused
+		false,           // exclusive
+		false,           // no-wait
+		nil,             // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	fmt.Println("Consumed results: ", msgs)
+}
+
+func fixturesProcessor() {
+
+	conn, err := amqp.Dial("amqp://liden:lID3n@10.132.0.28:5672/")
+	//conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"FIXTURES_QUEUE", // name
+		false,            // durable
+		false,            // delete when unused
+		false,            // exclusive
+		false,            // no-wait
+		nil,              // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	fmt.Println("Consumed fixture status: ", msgs)
+}
