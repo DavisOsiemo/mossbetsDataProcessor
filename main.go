ecb3ce2a4f5adc9887713f677b09746db2f05617
Davis Osiemo <davisossy@gmail.com>
2024-12-02 16:01:04 +0300

    VC

--- main.go
+++ main.go
@@ -258,7 +258,7 @@ func marketsConsumer(conn *amqp.Connection) {
 								//fmt.Println("Odds updated in DB:", string(oddsObjMarsh))
 							}
 						}
-						fmt.Println("Markets Added to DB for Fixture: ", marketSet.FixtureId)
+						fmt.Println("Markets Added to DB for Fixture: ", marketSet.FixtureId, ": ", markets.Name)
 						//log.Info().Msg("Market Set Added to DB " + markets.MarketType.Name + string(markets.Id))
 
 					}
