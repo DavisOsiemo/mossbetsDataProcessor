da034438644239e7e4806f954c09f9217eb70392
Davis Osiemo <davisossy@gmail.com>
2024-12-11 13:58:20 +0300

    Data

--- main.go
+++ main.go
@@ -324,7 +324,7 @@ func insertBatchIntoDB(messages []Odds) error {
 
 	// Add each message's data into the query
 	for _, record := range messages {
-		query += "(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?),"
+		query += "(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE odd_status=VALUES(odd_status), odds=VALUES(odds), prevous_odds=VALUES(prevous_odds), producer_id=VALUES(producer_id), alias=VALUES(alias), market_name=VALUES(market_name), status=VALUES(status), status_name=VALUES(status_name), odd_status=VALUES(odd_status),"
 		vals = append(vals, record.Outcome_id, record.Odd_status, record.Outcome_name, record.Match_id,
 			record.Odds, record.Prevous_odds, record.Direction, record.Producer_name, record.Market_id,
 			record.Producer_id, record.Producer_status, record.Market_name,
