74e55ac856b2b4fe429ef14f8e417000d4554f44
Davis Osiemo <davisossy@gmail.com>
2025-01-02 22:35:37 +0300

    Data

--- main.go
+++ main.go
@@ -269,7 +269,7 @@ func consumeFromRabbitMQ(msgs <-chan amqp.Delivery, queue chan Odds) {
 					var market_priority int
 					var alias_priority int
 
-					if markets.Id == 2 { //1x2
+					if markets.MarketType.Id == 2 { //1x2
 						if vals.Name == markets.Selections[0].Name {
 							alias = "1"
 						} else if vals.Name == markets.Selections[1].Name {
@@ -277,7 +277,7 @@ func consumeFromRabbitMQ(msgs <-chan amqp.Delivery, queue chan Odds) {
 						} else if vals.Name == markets.Selections[2].Name {
 							alias = "2"
 						}
-					} else if markets.Id == 7202 { //Double Chance
+					} else if markets.MarketType.Id == 7202 { //Double Chance
 						if vals.Name == markets.Selections[0].Name {
 							alias = "1X"
 						} else if vals.Name == markets.Selections[1].Name {
