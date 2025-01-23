e8ca7392eb025ac2bad6e95845a49f4d3a53016a
Davis Osiemo <davisossy@gmail.com>
2025-01-23 11:48:58 +0300

    Data fixes

--- main.go
+++ main.go
@@ -17,9 +17,9 @@ func failOnError(err error, msg string) {
 }
 
 const (
-	maxQueueSize   = 2000
+	maxQueueSize   = 500
 	maxWorkerCount = 1
-	batchSize      = 50 // Number of rows per batch
+	batchSize      = 10 // Number of rows per batch
 )
 
 func main() {
