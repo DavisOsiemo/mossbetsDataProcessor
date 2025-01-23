3bcf47939ed0fc822491f89174f51398f1a0bdf3
Davis Osiemo <davisossy@gmail.com>
2025-01-23 15:42:19 +0300

    Workers

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func failOnError(err error, msg string) {
 const (
 	maxQueueSize   = 2000
 	maxWorkerCount = 1
-	batchSize      = 15 // Number of rows per batch
+	batchSize      = 100 // Number of rows per batch
 )
 
 func main() {
