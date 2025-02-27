4ac38bbba26e26d7ffb9aab2ab4066a131ac978a
Davis Osiemo <davisossy@gmail.com>
2025-02-27 22:06:50 +0300

    Data Processor

--- main.go
+++ main.go
@@ -377,6 +377,18 @@ func consumeFromRabbitMQ(msgs <-chan amqp.Delivery, queue chan Odds) {
 						} else if vals.Name == markets.Selections[1].Name {
 							alias = "U"
 						}
+					} else if markets.Name == "Total Goals Over / Under 4.50" {
+						if vals.Name == markets.Selections[0].Name {
+							alias = "O"
+						} else if vals.Name == markets.Selections[1].Name {
+							alias = "U"
+						}
+					} else if markets.Name == "Total Goals Over / Under 5.50" {
+						if vals.Name == markets.Selections[0].Name {
+							alias = "O"
+						} else if vals.Name == markets.Selections[1].Name {
+							alias = "U"
+						}
 					} else {
 						alias = vals.Name
 					}
